2c8e8b1e62c7aa9c45d670662b9711d310da840d
Eduar Trujillo <eetgrisales@gmail.com>
2023-10-16 13:29:14 -0500

    Change in the html script tag to execute the code correctly with .mjs

---
+++ js/app.mjs
@@ -0,0 +1,9 @@
+import { listCourses } from "./variables.mjs";
+import { addCourse } from "./functions.mjs";
+
+loadEventListeners()
+
+function loadEventListeners() {
+    listCourses.addEventListener('click', addCourse);
+}
+
