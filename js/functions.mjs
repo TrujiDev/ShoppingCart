e8ce9b3a238a29f8f93da401334b2404fcec8620
Eduar Trujillo <eetgrisales@gmail.com>
2023-10-16 14:37:45 -0500

    New function that receives the course information by parameters and extracts it to save it in an object.

--- js/functions.mjs
+++ js/functions.mjs
@@ -1,8 +1,19 @@
 function addCourse(event) {
   event.preventDefault();
   if (event.target.classList.contains("add-cart")) {
-    console.log(event.target);
+    const selectedCourse = event.target.parentElement.parentElement;
+    readCourseData(selectedCourse);
   }
+}
+
+function readCourseData(course) {
+    const courseInfo = {
+        id: course.querySelector('a').getAttribute('data-id'),
+        image: course.querySelector('img').src,
+        price: course.querySelector('.price span').textContent,
+        quantity: 1,
+        title: course.querySelector('h4').textContent
+    }
 }
 
 export { addCourse };
