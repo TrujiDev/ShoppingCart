00b3e67bfd790d8b132ba43ba366ba22704ed1f2
Eduar Trujillo <eetgrisales@gmail.com>
2023-10-16 13:32:20 -0500

    Revert "Change in the html script tag to execute the code correctly with .mjs"
    This reverts commit 2c8e8b1e62c7aa9c45d670662b9711d310da840d.

--- js/functions.mjs
+++
@@ -1,8 +0,0 @@
-function addCourse(event) {
-  event.preventDefault();
-  if (event.target.classList.contains("add-cart")) {
-    console.log(event.target);
-  }
-}
-
-export { addCourse };
--- js/variables.mjs
+++
@@ -1,6 +0,0 @@
-const cart = document.querySelector("#cart");
-const cartContainer = document.querySelector("#list-cart tbody");
-const emptyCartBtn = document.querySelector("#empty-cart");
-const listCourses = document.querySelector("#list-courses");
-
-export { cart, cartContainer, emptyCartBtn, listCourses };
